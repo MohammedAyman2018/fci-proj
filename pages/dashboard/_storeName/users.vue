bbe0d9060d6dbe68329198e3d21c8d6b4ed9f159
MohammedAyman2018 <emidobibo20@gmail.com>
2021-12-18 15:47:58 +0200

    feat: delete user working.

--- pages/dashboard/_storeName/users.vue
+++ pages/dashboard/_storeName/users.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
+  <div class="container mx-auto px-2">
     <h2 class="text-2xl mb-3">كل العملاء</h2>
     <div v-for="user in displayedUsers" :key="user._id" class="user">
       <div class="flex justify-between items-center mb-4">
         <h3 class="text-lg">{{ user.name }}</h3>
         <div>
           <div data-tip="تعديل" class="tooltip">
-            <a href="#my-modal" class="btn btn-warning btn-square btn-xs">
+            <button
+              class="btn btn-warning btn-square btn-xs"
+              @click="openModal('edit-user', user)"
+            >
               <i class="ri-pencil-line"></i>
-            </a>
+            </button>
           </div>
           <div data-tip="تفاصيل أكثر" class="tooltip">
             <a href="#my-modal" class="btn btn-danger btn-square btn-xs">
@@ -16,21 +19,40 @@
             </a>
           </div>
           <div data-tip="حذف" class="tooltip">
-            <a href="#my-modal" class="btn btn-info btn-square btn-xs">
+            <button
+              class="btn btn-error btn-square btn-xs"
+              @click="openModal('delete-user', user)"
+            >
               <i class="ri-delete-bin-line"></i>
-            </a>
+            </button>
           </div>
         </div>
       </div>
       <div class="flex justify-between items-center text-xs">
         <p>الدولة: {{ user.country.name }}</p>
         <p>البريد الالكتروني: {{ user.email }}</p>
-        <p>رقم الهاتف: {{ user.phone }}</p>
+        <p>
+          رقم الهاتف: <bdi>{{ user.phone }}</bdi>
+        </p>
       </div>
     </div>
-
-    <div id="my-modal" class="modal">
+    <modal name="delete-user" scrollable height="auto">
       <div class="p-4">
+        <h2 class="text-xl font-bold">حذف العميل</h2>
+        <p>هل انت واثق انك تريد حذف العميل؟</p>
+
+        <div class="mt-5">
+          <button class="btn btn-error" @click="removeUser">نعم</button>
+          <button class="btn btn-error" @click="closeModal('delete-user')">
+            إلغاء
+          </button>
+        </div>
+      </div>
+    </modal>
+
+    <modal name="edit-user" adaptive height="auto" scrollable>
+      <div class="p-4">
+        <h2 class="text-xl font-bold">تعديل العميل</h2>
         <custom-input
           :label="'اسم المتجر'"
           :message="'اسم المتجر مطلوب'"
@@ -70,8 +92,7 @@
           @changed="user.password = $event"
         />
       </div>
-      <h2>تعديل العميل</h2>
-    </div>
+    </modal>
   </div>
 </template>
 
@@ -79,18 +100,59 @@
 import Vue from 'vue'
 import IUser from '@/interfaces/user'
 import CustomInput from '~/components/utils/CustomInput.vue'
+import PhoneInput from '~/components/utils/PhoneInput.vue'
 export default Vue.extend({
   name: 'UsersDashBoard',
-  components: { CustomInput },
+  components: { CustomInput, PhoneInput },
   data() {
     return {
+      user: {} as IUser,
       users: [] as IUser[],
       displayedUsers: [] as IUser[],
     }
   },
   async mounted() {
-    this.users = await this.$axios.$get('/users')
-    this.displayedUsers = this.users
+    await this.getUsers()
+  },
+  methods: {
+    async getUsers() {
+      try {
+        this.users = await this.$axios.$get('/users')
+        this.displayedUsers = this.users
+      } catch (error: any) {
+        this.$notify({
+          group: 'foo',
+          type: 'error',
+          title: error || '',
+        })
+      }
+    },
+    openModal(modalName, user) {
+      this.user = user
+      this.$modal.show(modalName)
+    },
+    editUser() {
+      console.log(this.user)
+      this.closeModal('edit-user')
+    },
+    async removeUser() {
+      try {
+        const res = await this.$axios.$delete(`/users/${this.user._id}`)
+        console.log(res)
+        await this.getUsers()
+        this.closeModal('delete-user')
+      } catch (error: any) {
+        this.$notify({
+          group: 'foo',
+          type: 'error',
+          title: error || '',
+        })
+      }
+    },
+    closeModal(modalName) {
+      this.user = {} as IUser
+      this.$modal.hide(modalName)
+    },
   },
 })
 </script>
