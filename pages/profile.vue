7a832753b583fa9834192eb5803a3ee7e3d70974
MohammedAyman2018 <emidobibo20@gmail.com>
2022-03-09 00:27:35 +0200

    feat: add profile page.

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,262 +1,124 @@
 <template>
-  <main class="profile-page">
-    <section class="relative block h-500-px">
-      <div
-        class="absolute top-0 w-full h-full bg-center bg-cover"
-        style="
-          background-image: url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80');
-        "
-      >
-        <span
-          id="blackOverlay"
-          class="w-full h-full absolute opacity-50 bg-black"
-        ></span>
-      </div>
-      <div
-        class="
-          top-auto
-          bottom-0
-          left-0
-          right-0
-          w-full
-          absolute
-          pointer-events-none
-          overflow-hidden
-          h-70-px
-        "
-        style="transform: translateZ(0px)"
-      >
-        <svg
-          class="absolute bottom-0 overflow-hidden"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="none"
-          version="1.1"
-          viewBox="0 0 2560 100"
-          x="0"
-          y="0"
-        >
-          <polygon
-            class="text-blueGray-200 fill-current"
-            points="2560 0 2560 100 0 100"
-          ></polygon>
-        </svg>
-      </div>
-    </section>
-    <section class="relative py-16 bg-blueGray-200">
-      <div class="container mx-auto px-4">
+  <main class="container mx-auto">
+    <h3 v-if="$auth.user" class="my-3 font-bold text-xl">
+      مرحباً
+      <bdi>{{ $auth.user.name }}</bdi>
+
+      <sup class="cursor-pointer" @click="showModal">
+        <i class="ri-pencil-line"> </i>
+      </sup>
+    </h3>
+
+    <div v-if="$auth.user" class="grid gap-3 grid-cols-2 lg:grid-cols-4">
+      <p>
+        المدينة:
+        <bdi>{{ $auth.user.country ? $auth.user.country : 'لا يوجد' }}</bdi>
+      </p>
+      <p>
+        البريد الإلكتروني: <bdi>{{ $auth.user.email }}</bdi>
+      </p>
+      <p>
+        رقم الهاتف:
+        <bdi>{{ $auth.user.phone ? $auth.user.phone : 'لا يوجد' }}</bdi>
+      </p>
+      <p>
+        العنوان:
+        <bdi>{{ $auth.user.address ? $auth.user.address : 'لا يوجد' }}</bdi>
+      </p>
+      <p>
+        تاريخ الميلاد:
+        <bdi>{{ $auth.user.dob ? $auth.user.dob : 'لا يوجد' }}</bdi>
+      </p>
+      <p>
+        تاريخ إنشاء الحساب:
+        <bdi>{{ $auth.user.createdAt.substr(0, 10) }}</bdi>
+      </p>
+    </div>
+
+    <div class="my-6">
+      <h3 class="my-3 font-bold text-xl">المفضلة:</h3>
+      <div v-if="fav.length > 0">
         <div
           class="
-            relative
-            flex flex-col
-            min-w-0
-            break-words
-            bg-white
-            w-full
-            mb-6
-            shadow-xl
-            rounded-lg
-            -mt-64
+            grid grid-cols-1
+            place-items-center
+            lg:grid-cols-2
+            xl:grid-cols-3
           "
         >
-          <div class="px-6">
-            <div class="flex flex-wrap justify-center">
-              <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                <div class="relative">
-                  <img
-                    alt="..."
-                    src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg"
-                    class="
-                      shadow-xl
-                      rounded-full
-                      h-auto
-                      align-middle
-                      border-none
-                      absolute
-                      -m-16
-                      -ml-20
-                      lg:-ml-16
-                      max-w-150-px
-                    "
-                  />
-                </div>
-              </div>
-              <div
-                class="
-                  w-full
-                  lg:w-4/12
-                  px-4
-                  lg:order-3 lg:text-right lg:self-center
-                "
-              >
-                <div class="py-6 px-3 mt-32 sm:mt-0">
-                  <button
-                    class="
-                      bg-pink-500
-                      active:bg-pink-600
-                      uppercase
-                      text-white
-                      font-bold
-                      hover:shadow-md
-                      shadow
-                      text-xs
-                      px-4
-                      py-2
-                      rounded
-                      outline-none
-                      focus:outline-none
-                      sm:mr-2
-                      mb-1
-                      ease-linear
-                      transition-all
-                      duration-150
-                    "
-                    type="button"
-                  >
-                    Connect
-                  </button>
-                </div>
-              </div>
-              <div class="w-full lg:w-4/12 px-4 lg:order-1">
-                <div class="flex justify-center py-4 lg:pt-4 pt-8">
-                  <div class="mr-4 p-3 text-center">
-                    <span
-                      class="
-                        text-xl
-                        font-bold
-                        block
-                        uppercase
-                        tracking-wide
-                        text-blueGray-600
-                      "
-                      >22</span
-                    ><span class="text-sm text-blueGray-400">Friends</span>
-                  </div>
-                  <div class="mr-4 p-3 text-center">
-                    <span
-                      class="
-                        text-xl
-                        font-bold
-                        block
-                        uppercase
-                        tracking-wide
-                        text-blueGray-600
-                      "
-                      >10</span
-                    ><span class="text-sm text-blueGray-400">Photos</span>
-                  </div>
-                  <div class="lg:mr-4 p-3 text-center">
-                    <span
-                      class="
-                        text-xl
-                        font-bold
-                        block
-                        uppercase
-                        tracking-wide
-                        text-blueGray-600
-                      "
-                      >89</span
-                    ><span class="text-sm text-blueGray-400">Comments</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="text-center mt-12">
-              <h3
-                class="
-                  text-4xl
-                  font-semibold
-                  leading-normal
-                  mb-2
-                  text-blueGray-700
-                  mb-2
-                "
-              >
-                Jenna Stones
-              </h3>
-              <div
-                class="
-                  text-sm
-                  leading-normal
-                  mt-0
-                  mb-2
-                  text-blueGray-400
-                  font-bold
-                  uppercase
-                "
-              >
-                <i
-                  class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"
-                ></i>
-                Los Angeles, California
-              </div>
-              <div class="mb-2 text-blueGray-600 mt-10">
-                <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i
-                >Solution Manager - Creative Tim Officer
-              </div>
-              <div class="mb-2 text-blueGray-600">
-                <i class="fas fa-university mr-2 text-lg text-blueGray-400"></i
-                >University of Computer Science
-              </div>
-            </div>
-            <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
-              <div class="flex flex-wrap justify-center">
-                <div class="w-full lg:w-9/12 px-4">
-                  <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
-                    An artist of considerable range, Jenna the name taken by
-                    Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                    performs and records all of his own music, giving it a warm,
-                    intimate feel with a solid groove structure. An artist of
-                    considerable range.
-                  </p>
-                  <a href="#pablo" class="font-normal text-pink-500"
-                    >Show more</a
-                  >
-                </div>
-              </div>
-            </div>
+          <product-card
+            v-for="product in fav"
+            :key="product._id"
+            :product="product"
+          />
+        </div>
+      </div>
+      <div v-else class="flex flex-col justify-center items-center">
+        <p class="font-semibold mb-4 text-center">
+          لا يوجد منتجات في المفضلة بعد
+        </p>
+        <nuxt-link class="btn btn-primary" to="/store/products">
+          تصفح منتجاتنا
+        </nuxt-link>
+      </div>
+    </div>
+
+    <div class="my-6">
+      <h3 class="my-3 font-bold text-xl">الطلبات:</h3>
+      <div v-if="orders.length > 0">
+        <div
+          class="
+            grid grid-cols-1
+            place-items-center
+            lg:grid-cols-2
+            xl:grid-cols-3
+          "
+        >
+          <div v-for="order in orders" :key="order._id">
+            {{ Object.keys(order) }}
           </div>
         </div>
       </div>
-      <footer class="relative bg-blueGray-200 pt-8 pb-6 mt-8">
-        <div class="container mx-auto px-4">
-          <div
-            class="
-              flex flex-wrap
-              items-center
-              md:justify-between
-              justify-center
-            "
-          >
-            <div class="w-full md:w-6/12 px-4 mx-auto text-center">
-              <div class="text-sm text-blueGray-500 font-semibold py-1">
-                Made with
-                <a
-                  href="https://www.creative-tim.com/product/notus-js"
-                  class="text-blueGray-500 hover:text-gray-800"
-                  target="_blank"
-                  >Notus JS</a
-                >
-                by
-                <a
-                  href="https://www.creative-tim.com"
-                  class="text-blueGray-500 hover:text-blueGray-800"
-                  target="_blank"
-                >
-                  Creative Tim</a
-                >.
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </section>
+      <div v-else class="flex flex-col justify-center items-center">
+        <p class="font-semibold mb-4 text-center">لا يوجد طلبات حتى الآن</p>
+        <nuxt-link class="btn btn-primary" to="/store/products">
+          تصفح منتجاتنا
+        </nuxt-link>
+      </div>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import productCard from '~/components/products/product-card.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: { productCard },
+  data() {
+    return {
+      fav: [],
+      orders: [],
+    }
+  },
+  async mounted() {
+    try {
+      const res = await Promise.all([
+        this.$axios.get('/users/all/fav/'),
+        this.$axios.get('/users/all/orders/'),
+      ])
+      this.fav = res[0].data
+      this.orders = res[1].data
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  methods: {
+    showModal() {
+      alert('we will edit this')
+    },
+  },
+})
 </script>
 
 <style scoped>
