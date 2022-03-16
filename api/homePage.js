4c37ce9e485554e8f6492cde57c27c3fef316bd4
MohammedAyman2018 <emidobibo20@gmail.com>
2022-03-17 00:18:27 +0200

    fix: reverse home page sort.

--- api/homePage.js
+++ api/homePage.js
@@ -7,9 +7,9 @@ router.get('/home', async (req, res) => {
   try {
     const query = getDeviceType(req, res)
     if (!req.query.storeName) return res.status(400).json({ msg: 'Provide Store Name query' })
-    const mostViewedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ views: 1 }).limit(10)
-    const mostOrderedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ ordered: 1 }).limit(10)
-    const mostRatedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ actualRating: 1 }).limit(10)
+    const mostViewedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ views: -1 }).limit(10)
+    const mostOrderedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ ordered: -1 }).limit(10)
+    const mostRatedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ actualRating: -1 }).limit(10)
 
     // TODO: Show Offers
     res.status(200).json({ mostViewedProducts, mostOrderedProducts, mostRatedProducts })
