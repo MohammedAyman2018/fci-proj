3fd7b24e5a0b50bfa274b4c2ab17449f203d21d1
MohammedAyman2018 <emidobibo20@gmail.com>
2022-03-19 16:17:19 +0200

    feat: send mostFav in home.

--- api/homePage.js
+++ api/homePage.js
@@ -10,10 +10,11 @@ router.get('/home', async (req, res) => {
     const mostViewedProducts = await Product.find(query, productProjection).sort({ views: -1 }).limit(10).populate('category')
     const mostOrderedProducts = await Product.find(query, productProjection).sort({ ordered: -1 }).limit(10).populate('category')
     const mostRatedProducts = await Product.find(query, productProjection).sort({ actualRating: -1 }).limit(10).populate('category')
+    const mostLovedProducts = await Product.find(query, productProjection).sort({ inFav: -1 }).limit(10).populate('category')
     const categories = await Category.find().limit(10)
 
     // TODO: Show Offers
-    res.status(200).json({ mostViewedProducts, mostOrderedProducts, mostRatedProducts, categories })
+    res.status(200).json({ mostViewedProducts, mostOrderedProducts, mostRatedProducts, categories, mostLovedProducts })
   } catch (error) {
     res.status(400).json({ msg: error })
   }
