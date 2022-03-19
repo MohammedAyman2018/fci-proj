153c7549f3951c0791248bceeca29eed6be25a40
MohammedAyman2018 <emidobibo20@gmail.com>
2022-03-19 15:27:51 +0200

    fix: home request and send category with it.

--- api/homePage.js
+++ api/homePage.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const { Category } = require('./Category/model')
 const { Product } = require('./Product/model')
 const { getDeviceType, productProjection } = require('./utils')
 const router = Router()
@@ -6,13 +7,13 @@ const router = Router()
 router.get('/home', async (req, res) => {
   try {
     const query = getDeviceType(req, res)
-    if (!req.query.storeName) return res.status(400).json({ msg: 'Provide Store Name query' })
-    const mostViewedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ views: -1 }).limit(10)
-    const mostOrderedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ ordered: -1 }).limit(10)
-    const mostRatedProducts = await Product.find({ ...query, storeName: req.query.storeName }, productProjection).populate('category').sort({ actualRating: -1 }).limit(10)
+    const mostViewedProducts = await Product.find(query, productProjection).sort({ views: -1 }).limit(10).populate('category')
+    const mostOrderedProducts = await Product.find(query, productProjection).sort({ ordered: -1 }).limit(10).populate('category')
+    const mostRatedProducts = await Product.find(query, productProjection).sort({ actualRating: -1 }).limit(10).populate('category')
+    const categories = await Category.find().limit(10)
 
     // TODO: Show Offers
-    res.status(200).json({ mostViewedProducts, mostOrderedProducts, mostRatedProducts })
+    res.status(200).json({ mostViewedProducts, mostOrderedProducts, mostRatedProducts, categories })
   } catch (error) {
     res.status(400).json({ msg: error })
   }
