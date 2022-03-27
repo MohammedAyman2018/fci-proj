960049e15fd72db35c54859ed18975dff6de8a92
MohammedAyman2018 <emidobibo20@gmail.com>
2022-03-27 15:19:26 +0200

    feat: add format.

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,39 +5,96 @@ export default {
     htmlAttrs: {
       'data-theme': 'light',
       dir: 'rtl',
-      lang: 'ar'
+      lang: 'ar',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'msapplication-TileColor', content: '#ffffff' },
-      { name: 'msapplication-TileImage', content: '/favicons/ms-icon-144x144.png' },
+      {
+        name: 'msapplication-TileImage',
+        content: '/favicons/ms-icon-144x144.png',
+      },
       { name: 'theme-color', content: '#ffffff' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes: '57x57', href: '/favicons/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '60x60', href: '/favicons/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes: '72x72', href: '/favicons/apple-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes: '76x76', href: '/favicons/apple-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: '/favicons/apple-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicons/apple-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes: '144x144', href: '/favicons/apple-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicons/apple-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-icon-180x180.png' },
-      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicons/android-icon-192x192.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicons/favicon-96x96.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
-      { rel: 'manifest', href: '/favicons/manifest.json' }
-    ]
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        href: '/favicons/apple-icon-57x57.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        href: '/favicons/apple-icon-60x60.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        href: '/favicons/apple-icon-72x72.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: '/favicons/apple-icon-76x76.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        href: '/favicons/apple-icon-114x114.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: '/favicons/apple-icon-120x120.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        href: '/favicons/apple-icon-144x144.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        href: '/favicons/apple-icon-152x152.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicons/apple-icon-180x180.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/favicons/android-icon-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicons/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/favicons/favicon-96x96.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicons/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/favicons/manifest.json' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/css/main.css'
-  ],
+  css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -47,7 +104,7 @@ export default {
     { src: './plugins/vue-slick-carousel.js' },
     '~/plugins/Autocomplete',
     '~/plugins/vuelidate',
-    '~/plugins/vue-js-modal.js'
+    '~/plugins/vue-js-modal.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -62,7 +119,7 @@ export default {
     '@braid/vue-formulate/nuxt',
   ],
   formulate: {
-    configPath: '~/plugins/formulate.config.js'
+    configPath: '~/plugins/formulate.config.js',
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -73,22 +130,22 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/toast',
-    '@nuxtjs/auth-next'
+    '@nuxtjs/auth-next',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     baseUrl: '/api',
     headers: {
-      device: 'web'
-    }
+      device: 'web',
+    },
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'ar'
-    }
+      lang: 'ar',
+    },
   },
 
   auth: {
@@ -102,36 +159,40 @@ export default {
       login: '/',
       logout: '/',
       callback: '/',
-      home: '/'
+      home: '/',
     },
     strategies: {
       local: {
         endpoints: {
           login: { url: '/users/auth', method: 'post', propertyName: 'token' },
           logout: false,
-          user: { url: '/users/me', method: 'get', propertyName: 'data' }
+          user: { url: '/users/me', method: 'get', propertyName: 'data' },
         },
         token: {
           required: true,
           name: 'x-auth-token',
-          type: ''
-        }
-      }
-    }
+          type: '',
+          maxAge: 1800000,
+        },
+      },
+    },
   },
   env: {
-    baseUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://fci-proj.herokuapp.com/api',
+    baseUrl:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000/api'
+        : 'https://fci-proj.herokuapp.com/api',
   },
 
   router: {
-    middleware: ['ssr-cookie']
+    middleware: ['ssr-cookie'],
   },
   serverMiddleware: {
-    '/api': '~/api'
+    '/api': '~/api',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: [/^vue2-google-maps($|\/)/]
-  }
+    transpile: [/^vue2-google-maps($|\/)/],
+  },
 }
