474cdb82760ca51412b8dcab8d5d12ebf2c538ac
MohammedAyman2018 <emidobibo20@gmail.com>
2022-03-27 15:18:16 +0200

    feat: add link to sign up and use formulate.

--- pages/login.vue
+++ pages/login.vue
@@ -5,41 +5,53 @@
         <div class="flex-col w-full justify-center hero-content lg:flex-row">
           <div class="text-center flex-grow lg:text-right">
             <h1 class="mb-5 text-5xl font-bold">أهلا بيك</h1>
-            <p class="mb-5">
-              اشتري كل احتياجاتك بافضل سعر ف السوق.
-            </p>
+            <p class="mb-5">اشتري كل احتياجاتك بافضل سعر ف السوق.</p>
           </div>
           <div
             class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"
           >
             <div class="card-body">
-              <FormulateInput
-                v-model="user.email"
-                type="email"
-                name="email"
-                label="البريد الإلكتروني"
-                placeholder="البريد الإلكتروني"
-                validation="required|email"
-              />
+              <FormulateForm
+                v-slot="{ isLoading }"
+                v-model="user"
+                @submit="userLogin"
+              >
+                <FormulateInput
+                  type="email"
+                  name="email"
+                  label="البريد الإلكتروني"
+                  placeholder="البريد الإلكتروني"
+                  validation="required|email"
+                />
 
-              <FormulateInput
-                v-model="user.password"
-                type="password"
-                name="password"
-                label="كلمة المرور"
-                placeholder="كلمة المرور"
-                validation="required"
-              />
-
-              <div class="form-control mt-6">
-                <button
-                  :class="{ 'btn-disabled': !valid }"
-                  class="btn w-full mt-4 btn-success"
-                  @click="userLogin"
-                >
-                  تسجيل
-                </button>
-              </div>
+                <FormulateInput
+                  type="password"
+                  name="password"
+                  label="كلمة المرور"
+                  placeholder="كلمة المرور"
+                  validation="required"
+                />
+                <div class="mt-4 text-center">
+                  <p class="text-sm">
+                    ليس لديك حساب؟
+                    <nuxt-link
+                      to="/sign-up"
+                      class="text-blue-600 hover:underline"
+                    >
+                      أنشئ حسابك.</nuxt-link
+                    >
+                  </p>
+                </div>
+                <div class="form-control mt-6">
+                  <FormulateInput
+                    type="submit"
+                    :wrapper-class="['w-full']"
+                    :input-class="['btn-success', 'w-full', 'btn']"
+                    :disabled="isLoading || !valid"
+                    :label="isLoading ? 'جاري التسجيل' : 'تسجيل'"
+                  />
+                </div>
+              </FormulateForm>
             </div>
           </div>
         </div>
