6697c47210d495731732cd5d3a550f61c4c7994a
MohammedAyman2018 <emidobibo20@gmail.com>
2022-04-02 00:04:41 +0200

    feat: new order to the new schema.

--- api/Orders/controller.js
+++ api/Orders/controller.js
@@ -11,25 +11,50 @@ export const getAllOrders = async (req, res) => {
   }
 }
 
-
 export const addOrder = async (req, res) => {
   try {
     const { error } = validate(req.body)
-    if (error) { return res.status(400).send({ msg: error.details[0].message }) }
-    const order = await Order.create(req.body)
-    const user = await User.findById(req.user.id)
-    user.orders.push(order._id)
-    for (let i = 0; i < req.body.items.length; i++) {
-      const item = req.body.items[i];
-      const productInDb = await Product.findById(item._id)
-      productInDb.ordered++
-      if (productInDb.amount && productInDb.amount.amountType === 'limited') {
-        productInDb.amount.available -= item.amount
-      }
-      await productInDb.save()
+
+    if (error) {
+      return res.status(400).send({ msg: error.details[0].message })
     }
-    await user.save()
-    return res.status(200).json(order)
+    const grouped = req.body.items.reduce(function (r, a) {
+      r[a.storeName] = r[a.storeName] || []
+      r[a.storeName].push(a)
+      return r
+    }, Object.create(null))
+
+    for (const store in grouped) {
+      if (Object.hasOwnProperty.call(grouped, store)) {
+        const arrOfItems = grouped[store]
+        // create order
+        let order = {
+          state: req.body.state,
+          user: req.body.user,
+          items: arrOfItems,
+          storeName: store,
+        }
+        order = await Order.create(order)
+        // add order to user orders
+        const user = await User.findById(req.user.id)
+        user.orders.push(order._id)
+        await user.save()
+        // increase ordered and decrease amount
+        for (let index = 0; index < arrOfItems.length; index++) {
+          const item = arrOfItems[index]
+          const productInDb = await Product.findById(item._id)
+          productInDb.ordered++
+          if (
+            productInDb.amount &&
+            productInDb.amount.amountType === 'limited'
+          ) {
+            productInDb.amount.available -= item.amount
+          }
+          await productInDb.save()
+        } // end for
+      } // end if
+    } // end for
+    return res.status(200).json({ msg: 'تم الطلب بنجاح' })
   } catch (err) {
     res.status(400).json({ msg: err.message })
   }
