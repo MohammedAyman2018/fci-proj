35b548fd4caf7268e8ba99a040d3eb3ae14d3e20
MohammedAyman2018 <emidobibo20@gmail.com>
2022-04-03 23:23:22 +0200

    feat: change contact us form.

--- api/ContactUs/model.js
+++ api/ContactUs/model.js
@@ -1,19 +1,26 @@
 const mongoose = require('mongoose')
-const Schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const Schema = new mongoose.Schema(
+  {
+    msgType: {
+      type: String,
+      enum: ['سؤال', 'شكوى', 'اقتراح'],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    msg: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true
-  },
-  msg: {
-    type: String,
-    required: true
-  },
-},
-  { timestamps: true })
+  { timestamps: true }
+)
 const ContactUs = mongoose.model('contactusform', Schema)
 
 module.exports.ContactUs = ContactUs
