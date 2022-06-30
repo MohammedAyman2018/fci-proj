b50307577e055387458a57537816dc6548758664
Mohammed_Ayman <emidobibo20@gmail.com>
2022-06-30 03:16:42 +0200

    fix: tooltip wasn't working.

--- pages/dashboard/_storeName/orders/index.vue
+++ pages/dashboard/_storeName/orders/index.vue
@@ -8,8 +8,19 @@
         { label: 'رقم العميل', field: 'user.tel', sortable: false },
         { label: 'طريقة الدفع', field: 'user.payment' },
         { label: 'حالة الطلب', field: 'state' },
-        { label: 'تاريخ  الطلب', field: 'createdAt', type:'date',  dateInputFormat: `yyyy-MM-dd'T'HH:mm:ss.SSS'Z'`, dateOutputFormat: 'MMM dd yyyy', },
-        { label: 'العمليات المتاحة', field: 'operations', sortable: false, globalSearchDisabled: true, },
+        {
+          label: 'تاريخ  الطلب',
+          field: 'createdAt',
+          type: 'date',
+          dateInputFormat: `yyyy-MM-dd'T'HH:mm:ss.SSS'Z'`,
+          dateOutputFormat: 'MMM dd yyyy',
+        },
+        {
+          label: 'العمليات المتاحة',
+          field: 'operations',
+          sortable: false,
+          globalSearchDisabled: true,
+        },
       ]"
       :rows="allOrders"
       :rtl="true"
@@ -18,7 +29,7 @@
     >
       <template slot="table-row" slot-scope="props">
         <span v-if="props.column.field == 'operations'" class="flex">
-          <div data-tip="التفاصيل" class="tooltip mx-1">
+          <b-tooltip label="التفاصيل" class="mx-1">
             <button
               class="btn btn-warning btn-square btn-xs"
               @click="
@@ -29,7 +40,7 @@
             >
               <i class="ri-file-chart-line"></i>
             </button>
-          </div>
+          </b-tooltip>
         </span>
       </template>
 
