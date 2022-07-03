6554fc20bdb634f863f466410aae6e567fd4a885
Mohammed_Ayman <emidobibo20@gmail.com>
2022-07-03 18:16:06 +0200

    style: center contacts page.

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,37 +1,45 @@
 <template>
   <div>
     <div class="container mx-2">
-      <h3 class="is-size-4 has-text-weight-bold">الشكاوى والمقترحات</h3>
-      <div class="columns is-multiline">
-        <b-field class="column is-12" label="الاسم">
-          <b-input v-model="name" value="اسم المتجر" />
-        </b-field>
-        <b-field class="column is-12" label="رقم هاتف للتواصل">
-          <b-input v-model="phone" value="رقم هاتف للتواصل" />
-        </b-field>
-        <b-field class="column is-12" label="ما هو نوع الرسالة">
-          <b-select v-model="msgType" expanded placeholder="اختر نوع الرسالة">
-            <option
-              v-for="option in ['سؤال', 'شكوى', 'اقتراح']"
-              :key="option"
-              :value="option"
-            >
-              {{ option }}
-            </option>
-          </b-select>
-        </b-field>
-        <b-field class="column is-12" label="الرسالة">
-          <b-input v-model="msg" maxlength="200" type="textarea"></b-input>
-        </b-field>
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h3 class="is-size-4 has-text-weight-bold">الشكاوى والمقترحات</h3>
+          <div class="columns is-multiline">
+            <b-field class="column is-12" label="الاسم">
+              <b-input v-model="name" value="اسم المتجر" />
+            </b-field>
+            <b-field class="column is-12" label="رقم هاتف للتواصل">
+              <b-input v-model="phone" value="رقم هاتف للتواصل" />
+            </b-field>
+            <b-field class="column is-12" label="ما هو نوع الرسالة">
+              <b-select
+                v-model="msgType"
+                expanded
+                placeholder="اختر نوع الرسالة"
+              >
+                <option
+                  v-for="option in ['سؤال', 'شكوى', 'اقتراح']"
+                  :key="option"
+                  :value="option"
+                >
+                  {{ option }}
+                </option>
+              </b-select>
+            </b-field>
+            <b-field class="column is-12" label="الرسالة">
+              <b-input v-model="msg" maxlength="200" type="textarea"></b-input>
+            </b-field>
 
-        <b-button
-          type="is-primary"
-          :disabled="!valid"
-          expanded
-          @click="sendContactForm"
-        >
-          إرسال
-        </b-button>
+            <b-button
+              type="is-primary"
+              :disabled="!valid"
+              expanded
+              @click="sendContactForm"
+            >
+              إرسال
+            </b-button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
