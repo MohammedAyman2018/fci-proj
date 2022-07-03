f2e9ec13ec8fe6aa0342a737fcac741ac8e0d488
Mohammed_Ayman <emidobibo20@gmail.com>
2022-07-03 18:15:38 +0200

    feat: Add support link.

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,9 @@
       <Navbar />
     </client-only>
     <Nuxt />
-    <nuxt-link to="/contact-us"> تواصل معنا </nuxt-link>
+    <nuxt-link class="contact-us" to="/contact-us">
+      <img width="50px" src="@/assets/support.png" alt="تواصل معنا" />
+    </nuxt-link>
   </div>
 </template>
 
@@ -12,3 +14,11 @@
 import 'remixicon/fonts/remixicon.css'
 export default {}
 </script>
+
+<style scoped>
+.contact-us {
+  position: fixed;
+  bottom: 10px;
+  right: 5%;
+}
+</style>
